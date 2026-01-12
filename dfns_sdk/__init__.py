de5acbf6a8dcd2dd59b25a7d0bdda4353015b6fe
Noah Cornwell <noah@dfns.co>
2026-01-12 09:40:14 -0500

    feat: move to dynamic hatch-vcs

--- dfns_sdk/__init__.py
+++ dfns_sdk/__init__.py
@@ -1,5 +1,10 @@
 """Dfns Python SDK - Auto-generated from OpenAPI specification."""
 
+try:
+    from ._version import __version__
+except ImportError:
+    __version__ = "0.0.0-dev"
+
 from .client import DfnsClient
 from .delegated_client import DfnsDelegatedClient
 from .types import DfnsClientConfig, DfnsDelegatedClientConfig, DfnsError
@@ -11,6 +16,7 @@ from .base_auth_api import (
 )
 
 __all__ = [
+    "__version__",
     "DfnsClient",
     "DfnsDelegatedClient",
     "DfnsClientConfig",
